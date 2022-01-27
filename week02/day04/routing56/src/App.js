837638669030f8c0d1dd486569cf4f0483b92480
Swen Mulderij <swenmulderij@gmail.com>
2022-01-27 14:25:08 +0100

    after afternoon lecture

--- week02/day04/routing56/src/App.js
+++ week02/day04/routing56/src/App.js
@@ -1,19 +1,38 @@
 import "./App.css";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import { Routes, Route, Link } from "react-router-dom";
+import PokemonPage from "./Pages/PokemonPage";
+import { Routes, Route, NavLink, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Link to="/">home</Link>
+        <NavLink className={"navItem"} to="/">
+          home
+        </NavLink>
         <br />
-        <Link to="/about">about</Link>
+        <NavLink className={"navItem"} to="/about">
+          about
+        </NavLink>
+        <br />
+        <NavLink className={"navItem"} to="/pokemon/psyduck">
+          psyduck
+        </NavLink>
+        <br />
+        <NavLink className={"navItem"} to="/pokemon/pikachu">
+          pikachu
+        </NavLink>
+        <br />
+        <NavLink className={"navItem"} to="/pokemon/charizard">
+          charizard
+        </NavLink>
+        <br />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/pokemon/:name" element={<PokemonPage />} />
       </Routes>
     </div>
   );
