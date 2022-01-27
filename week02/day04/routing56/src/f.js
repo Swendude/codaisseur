21b51842b2d55124a58744f99844aae46e70893b
Swen Mulderij <swenmulderij@gmail.com>
2022-01-27 16:58:16 +0100

    Added an example on filter

--- week02/day04/routing56/src/f.js
+++ week02/day04/routing56/src/f.js
@@ -1,6 +1,22 @@
-function add(x, y) {
-  return x + y;
-}
+const fruits = [
+  { name: "banana", qty: 50 },
+  { name: "boconut", qty: 150 },
+  { name: "mango", qty: 5000 },
+  { name: "mandarin", qty: 500 },
+  { name: "watermelon", qty: 100 },
+  { name: "figs", qty: 0.5 },
+];
+const searchVal = "b";
 
-console.log(add(8, 9));
-console.log(add(1, 3));
+const result = fruits.filter((fruit) => {
+  return fruit.qty < 5;
+});
+
+console.log(result);
+
+// function add(x, y) {
+//   return x + y;
+// }
+
+// console.log(add(8, 9));
+// console.log(add(1, 3));
