b5cddbdfb45baed5dbfe625340e709ecc9684a1b
Swen Mulderij <swenmulderij@gmail.com>
2022-04-19 14:31:47 +0200

    After second lecture

--- week02/day01/fruits/src/App.js
+++ week02/day01/fruits/src/App.js
@@ -1,26 +1,31 @@
 import "./App.css";
 import NavigationBar from "./NavigationBar";
 import FruitCard from "./FruitCard";
+import fruitStock from "./fruitStock.json";
+// function objectToCard (object) {
+
+// } /// This is annoying
+
 function App() {
+  // const removeFruit = () => {
+  //   fruitsStock.pop();
+  //   console.log(fruitsStock);
+  // };
+
   return (
     <div className="App">
       <NavigationBar />
       <p>Welcome to fruits.com</p>
-      <FruitCard
-        name="Banana"
-        imgSrc="https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg"
-        description="Sweet yellow fruit"
-      />
-      <FruitCard
-        name="Peaches"
-        imgSrc="https://static.libertyprim.com/files/familles/peche-large.jpg?1574630286"
-        description="Sweet pink fruit"
-      />
-      <FruitCard
-        name="Pineapples"
-        imgSrc="https://upload.wikimedia.org/wikipedia/commons/7/74/%E0%B4%95%E0%B5%88%E0%B4%A4%E0%B4%9A%E0%B5%8D%E0%B4%9A%E0%B4%95%E0%B5%8D%E0%B4%95.jpg"
-        description="Don't put it on pizza"
-      />
+      {/* <button onClick={() => removeFruit()}>Remove fruit</button> */}
+      {fruitStock
+        .filter((fruit) => !fruit.name.startsWith("A"))
+        .map((fruit) => (
+          <FruitCard
+            name={fruit.name}
+            imgSrc={fruit.image}
+            description={fruit.description}
+          /> // IF WE ARE INSIDE AN HTML (JSX) ELEMENT WE NEED TO USE BRACKETS AGAIN!!!
+        ))}
     </div>
   );
 }
