9c1995d3c6c89f207dc72d8bed76687d35abc7ac
Swen Mulderij <swenmulderij@gmail.com>
2022-04-22 14:26:05 +0200

    After battleplan

--- week02/day04/routing/src/pages/HomePage.js
+++ week02/day04/routing/src/pages/HomePage.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const HomePage = () => {
-  return <h1 className="redText">Home</h1>;
+  const [getPokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
+      console.log(response.data.results);
+      setPokemons(response.data.results);
+    };
+    fetchPokemon();
+  }, []);
+
+  return (
+    <div>
+      {getPokemons.map((pokemon) => (
+        <div>
+          <p>{pokemon.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
