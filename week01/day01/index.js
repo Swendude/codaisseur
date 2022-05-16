c90b3e1af676df196dd5f94627ed4b89a71a8515
Swen Mulderij <swenmulderij@gmail.com>
2022-05-16 16:30:28 +0200

    after hello js 2

--- week01/day01/index.js
+++ week01/day01/index.js
@@ -1,4 +1,47 @@
-const cow = true;
-const sheep = false;
-const pig = "true";
-console.log(cow + 1);
+function double(cow) {
+  return cow * 2;
+}
+
+const double = (cow) => {
+  return cow * 2;
+};
+
+// function checkBeer(age, country) {
+//   if (country === "NL" && age >= 18) {
+//     return true;
+//   }
+
+//   if (country === "GE" && age >= 17) {
+//     return true;
+//   }
+
+//   if (country === "BE" && age >= 16) {
+//     return true;
+//   }
+//   return false;
+// }
+// Object solution
+// const student = {
+//   name: "Seval",
+//   class: 59,
+//   excited: true,
+//   pets: ["Kara", "Mickey"]
+// };
+
+// const myArray = ["A", "B", "C"];
+
+// console.log("Student is: ", student);
+// console.log(student * myArray + 7);
+
+// // Array solution
+// const studentArr = ['Seval', 59, true];
+
+// console.log(student['name'])
+// console.log(studentArr[0])
+
+// console.log(student['class'])
+// // const sheep = [1, 12, "Swen", "Codaisseur", 42, 82, ['Karla', 'Matias', 'David', 'Maria']]
+
+// // // const pig = sheep[9];
+// // const subArray = sheep[6]
+// // console.log(subArray.length)
