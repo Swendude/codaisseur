7d380b485f00e6f5762b98f2aabb2ec74cb4eec3
Swen Mulderij <swenmulderij@gmail.com>
2022-07-07 10:38:41 +0200

    After react routing 1 60

--- week02/day04/routing/src/App.js
+++ week02/day04/routing/src/App.js
@@ -1,26 +1,18 @@
 import "./App.css";
-import { Route, Routes, Link } from "react-router-dom";
-import AboutPage from "./pages/AboutPage";
+import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import GreetingPage from "./pages/GreetingPage";
-// I would like to go to /about and see a different page
+import ContactsPage from "./pages/ContactsPage";
 
-// IMPORTANT!!!! INSIDE 'ELEMENT' YOU NEED TO PUT A WHOLE COMPONENT (NOT JUST THE NAME)
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <div className="nav">
+        <Link to="/">home</Link>
+        <Link to="/contacts">contacts</Link>
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/greeting/:cow/:sheep" element={<GreetingPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
       </Routes>
     </div>
   );
