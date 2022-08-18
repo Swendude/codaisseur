698a10880f759fad211fb092c5f3d7fcdaf41fa6
Swen Mulderij <swenmulderij@gmail.com>
2022-08-18 12:24:06 +0200

    After clients 1 class-61

--- week01/day04/client.js
+++ week01/day04/client.js
@@ -1,21 +1,27 @@
+// We want to make an HTTP GET request from javascript
+// WISH: const response = get("https://pokeapi.co/api/v2/pokemon/pikachu")
+
+// const double = (x) => {
+//   return x * 2;
+// };
+
+// const cow = 5;
+// const sheep = double(cow);
+// console.log(sheep);
+
 const axios = require("axios");
 
-const getAnimals = async () => {
-  try {
-    const response = await axios.get("http://localhost:5065/animals");
-    console.log(response);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+const double = (x) => x * 2;
 
-const getAnimal = async (animal_id) => {
-  const response = await axios.get(
-    `http://localhost:5065/animals/${animal_id}`
-  );
+const getPikachuArrow = async () => {
+  const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu");
   console.log(response.data);
 };
 
+async function getPikachu() {
+  const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu");
+  console.log(response.data);
+}
 console.log("first");
-getAnimals();
+getPikachu();
 console.log("second");
