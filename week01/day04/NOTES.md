dbb0fe8bcbad4341771955f841ac7915cde45a62
Swen Mulderij <swenmulderij@gmail.com>
2022-10-27 13:20:19 +0200

    After 63 clients

---
+++ week01/day04/NOTES.md
@@ -0,0 +1,9 @@
+HTTP
+Hyper Text Transfer Protocol
+
+HTML
+Hyper Text Markup Language
+
+Http has an adres:
+GET localhost:4000/animals
+GET localhost:4000/animals/1
--- week01/day04/client.js
+++ week01/day04/client.js
@@ -1,41 +1,18 @@
-// We want to make an HTTP GET request from javascript
-// WISH: const response = get("https://pokeapi.co/api/v2/pokemon/pikachu")
-
-// const double = (x) => {
-//   return x * 2;
-// };
-
-// const cow = 5;
-// const sheep = double(cow);
-// console.log(sheep);
-
 const axios = require("axios");
 
-const getPikachu = async () => {
-  const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu");
-  console.log(response.data);
+const getAllPokemon = async () => {
+  const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+  // console.log(response.data);
+  console.log("Pokemon");
 };
 
-const getCharacter = async (characterId) => {
-  const response = await axios.get(
-    `http://localhost:4000/characters/${characterId}`
-  );
-  console.log(response.data);
+const getGoogle = async () => {
+  const response = await axios.get("https://www.google.com");
+  // console.log(response.data);
+  console.log("google");
 };
 
-// ("http://localhost:4000/data/male");
-// ("http://localhost:4000/data/female");
-const getGender = async (patientGender) => {
-  const response = await axios.get(
-    `http://localhost:4000/data/${patientGender}`
-  );
-  console.log(response.data);
-};
+getGoogle();
+getAllPokemon();
 
-getGender("Male");
-
-getCharacter(13);
-
-// console.log("first");
-// getPikachu();
-// console.log("second");
+// console.log("Hello, world!");
