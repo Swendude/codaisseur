1fd6c038e144d8a79fabd449e259705f93622e83
Swen Mulderij <swenmulderij@gmail.com>
2022-11-17 10:30:59 +0100

    After 63 styled

--- week04/day04/src/App.js
+++ week04/day04/src/App.js
@@ -1,20 +1,38 @@
 import styled from "styled-components";
+import "./app.css";
 
-import CoolButton from "./components/CoolButton";
-
-const MyGreeting = styled.p`
-  font-size: ${(props) => (props.big ? "x-large" : "small")};
+const RedText = styled.p`
   color: red;
-  text-decoration: underline;
+  background: yellow;
+  font-size: ${(props) => (props.size === "small" ? "1rem" : "2rem")};
+  border: 4px solid green;
+`;
+
+const BoldRedText = styled(RedText)`
+  font-weight: bolder;
+`;
+
+const AppButton = styled.button`
+  width: 10rem;
+  aspect-ratio: 16/9;
+  border: 2px solid red;
+  border-radius: 5px;
+  color: ${(props) => (props.role === "secondary" ? "white" : "red")};
+  background-color: ${(props) =>
+    props.role === "secondary" ? "red" : "white"};
+  font-weight: bold;
 `;
 
 function App() {
   return (
     <div className="App">
-      <MyGreeting>Hello, world!</MyGreeting>
-      <MyGreeting big={true}>Hello, world!</MyGreeting>
-      <CoolButton />
-      <CoolButton variant="secondary" />
+      <h1>Hello Styled Components</h1>
+      <RedText>Today we are learning about styling our components</RedText>
+      <BoldRedText>
+        Today we are learning about styling our components
+      </BoldRedText>
+      <AppButton role="secondary">Click me!</AppButton>
+      <AppButton>Click me!</AppButton>
     </div>
   );
 }
